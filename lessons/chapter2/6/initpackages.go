bec08972e2308a86af1ac61bc0f146d404d3b3ad
Roman <churomann@gmail.com>
2018-04-17 02:34:05 +0300

    undrsnd popcount

--- lessons/chapter2/6/initpackages.go
+++ lessons/chapter2/6/initpackages.go
@@ -5,27 +5,6 @@ import (
     "fmt"
 )
 
-// var a = b + c
-// var b = f()
-// var c = 1
-//
-// func f() int { return c + 1 }
-//
-// func init() {
-//     fmt.Println("Hello")
-// }
-//
-// func init() {
-//     fmt.Println("world")
-// }
-
 func main() {
-    fmt.Printf("\nres: %d\n", popcount.PopCount(1000))
-    // var s byte = 500
-    // fmt.Println(byte(s))
-    // fmt.Println(popcount.Pc)
-    // var s byte = 255
-    // fmt.Printf("\n%b\n", s<<0)
-    // fmt.Printf("\n%b\n", s<<1)
-    // fmt.Printf("\n%b\n", s>>1)
+    fmt.Println(popcount.PopCount(1000))
 }
--- lessons/chapter2/6/popcount/popcount.go
+++ lessons/chapter2/6/popcount/popcount.go
@@ -1,36 +1,14 @@
 package popcount
 
-import "fmt"
-
 var Pc [256]byte
 
 func init() {
     for i := range Pc {
         Pc[i] = Pc[i/2] + byte(i&1)
     }
-    fmt.Println()
-    fmt.Println(Pc)
-    fmt.Println()
 }
 
 func PopCount(x uint64) int {
-    fmt.Printf("\nX:\n%b\n", x)
-
-    fmt.Print("B:")
-    fmt.Printf("\n%b", Pc[byte(x>>(0*8))])
-    fmt.Printf("\n%b", Pc[byte(x>>(1*8))])
-    fmt.Printf("\n%b", Pc[byte(x>>(2*8))])
-    fmt.Printf("\n%b", Pc[byte(x>>(3*8))])
-    fmt.Printf("\n%b", Pc[byte(x>>(4*8))])
-    fmt.Printf("\n%b", Pc[byte(x>>(5*8))])
-    fmt.Printf("\n%b", Pc[byte(x>>(6*8))])
-    fmt.Printf("\n%b", Pc[byte(x>>(7*8))])
-    fmt.Println()
-    fmt.Println(byte(x))
-
-
-
-
     return int(
         Pc[byte(x>>(0*8))] +
         Pc[byte(x>>(1*8))] +
@@ -41,3 +19,10 @@ func PopCount(x uint64) int {
         Pc[byte(x>>(6*8))] +
         Pc[byte(x>>(7*8))])
 }
+
+// func test(d int) int {
+//     fmt.Printf("%b\n", byte(d))
+//     fmt.Printf("%b\n", byte(d>>8))
+//     fmt.Printf("%b\n", d)
+//     return 5
+// }

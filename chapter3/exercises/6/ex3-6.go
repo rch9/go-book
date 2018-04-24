94ae250910af0b9d0279440629b770327f994a9e
Roman <churomann@gmail.com>
2018-04-24 20:18:07 +0300

    added ex 6

--- chapter3/exercises/6/ex3-6.go
+++ chapter3/exercises/6/ex3-6.go
@@ -1,65 +1,67 @@
 package main
 
 import (
-    "image"
-    "image/color"
-    "image/png"
-    "math/cmplx"
-    "os"
+	"image"
+	"image/color"
+	"image/png"
+	"math/cmplx"
+	"os"
 )
 
-// how to make const param in func
+// FIXME: THERE ARE MANY ERRORS...
+
 func main() {
-    const (
-        xmin, ymin, xmax, ymax = -2, -2, +2, +2
-        width, height          = 1024, 1024
-    )
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
 
-    img := image.NewRGBA(image.Rect(0, 0, width, height))
-    for py := 0; py < height; py += 4 {
-        y0 := float64(py + 0) / height * (ymax - ymin) + ymin
-        y1 := float64(py + 1) / height * (ymax - ymin) + ymin
-        y2 := float64(py + 2) / height * (ymax - ymin) + ymin
-        y3 := float64(py + 3) / height * (ymax - ymin) + ymin
-        for px := 0; px < width; px += 4 {
-            x0 := float64(px + 0) / width *(xmax - xmin) + xmin
-            x1 := float64(px + 1) / width *(xmax - xmin) + xmin
-            x2 := float64(px + 2) / width *(xmax - xmin) + xmin
-            x3 := float64(px + 3) / width *(xmax - xmin) + xmin
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py += 2 {
+		y0 := float64(py+0)/height*(ymax-ymin) + ymin
+		y1 := float64(py+1)/height*(ymax-ymin) + ymin
+		// y2 := float64(py+2)/height*(ymax-ymin) + ymin
+		// y3 := float64(py+3)/height*(ymax-ymin) + ymin
+		for px := 0; px < width; px += 2 {
+			x0 := float64(px+0)/width*(xmax-xmin) + xmin
+			x1 := float64(px+1)/width*(xmax-xmin) + xmin
+			// x2 := float64(px+2)/width*(xmax-xmin) + xmin
+			// x3 := float64(px+3)/width*(xmax-xmin) + xmin
 
-            z0 := complex(x0, y0)
-            z1 := complex(x1, y1)
-            z2 := complex(x2, y2)
-            z3 := complex(x3, y3)
-            img.Set(px, py, calcMidColor([]color.Color{
-                mandelbrot(z0), mandelbrot(z1), mandelbrot(z2), mandelbrot(z3)}))
-        }
-    }
-    png.Encode(os.Stdout, img) // should check errors
+			z0 := complex(x0, y0)
+			z1 := complex(x0, y1)
+			z2 := complex(x1, y0)
+			z3 := complex(x1, y1)
+			img.Set(px, py, calcMidColor([]color.Color{
+				mandelbrot(z0), mandelbrot(z1), mandelbrot(z2), mandelbrot(z3)}))
+		}
+	}
+	png.Encode(os.Stdout, img) // should check errors
 }
 
 func calcMidColor(colors []color.Color) color.Color {
-    var r, g, b, a uint32
-    for _, c := range colors {
-        cr, cg, cb, ca := c.RGBA()
-        r, g, b, a = r + cr, g + cg, b + cb, a + ca
-    }
-    len := uint32(len(colors))
-    r, g, b, a = r / len, g / len, b / len, a / len
+	var r, g, b, a uint32
+	for _, c := range colors {
+		cr, cg, cb, ca := c.RGBA()
+		r, g, b, a = r+cr, g+cg, b+cb, a+ca
+	}
+	len := uint32(len(colors))
+	r, g, b, a = r/len, g/len, b/len, a/len
 
-    return color.RGBA{uint8(r), uint8(g), uint8(b), uint8(a)}
+	return color.RGBA{uint8(r), uint8(g), uint8(b), uint8(a)}
 }
 
 func mandelbrot(z complex128) color.Color {
-    const iterations  = 200
-    const contrast = 15
+	const iterations = 200
+	const contrast = 15
 
-    var v complex128
-    for n := uint8(0); n < iterations; n++ {
-        v = v*v + z
-        if cmplx.Abs(v) > 2 {
-            return color.YCbCr{255 - contrast*n, contrast*n, 255 - contrast*n}
-        }
-    }
-    return color.Black
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			//return color.YCbCr{255 - contrast*n, contrast * n, 255 - contrast*n}
+			return color.RGBA{255 - contrast*n, 0, 255 - contrast*n, 255}
+		}
+	}
+	return color.Black
 }
--- chapter3/exercises/6/ex3-6_test.go
+++ chapter3/exercises/6/ex3-6_test.go
@@ -1,42 +1,38 @@
 package main
 
 import (
-    "testing"
-    "image/color"
+	"image/color"
+	"testing"
 )
 
 func TestSupersampling(t *testing.T) {
-    t.Log("TestSupersampling stated")
-    // var color int
-    //
-    // color = getColorFromRangeByNormedAvg(0)
-    // if color != 0x0000ff {
-    //     t.Errorf("color0 was incorrect, got: %#.6x, want: %#.6x.", color, 0x0000ff)
-    // }
-    //
-    // color = getColorFromRangeByNormedAvg(1)
-    // if color != 0xff0000 {
-    //     t.Errorf("color1 was incorrect, got: %#.6x, want: %#.6x.", color, 0xff0000)
-    // }
+	t.Log("TestSupersampling stated")
+	// var color int
+	//
+	// color = getColorFromRangeByNormedAvg(0)
+	// if color != 0x0000ff {
+	//     t.Errorf("color0 was incorrect, got: %#.6x, want: %#.6x.", color, 0x0000ff)
+	// }
+	//
+	// color = getColorFromRangeByNormedAvg(1)
+	// if color != 0xff0000 {
+	//     t.Errorf("color1 was incorrect, got: %#.6x, want: %#.6x.", color, 0xff0000)
+	// }
 }
 
 func TestCalcMidColor(t *testing.T) {
-    t.Log("TestCalcMidColor stated")
-    var res, test color.RGBA
+	t.Log("TestCalcMidColor stated")
+	var res, test color.Color
 
-    res = calcMidColor(color.RGBA{1,1,1,1}, color.RGBA{3,3,3,3})
-    test = color.RGBA{2,2,2,2}
-    if res.A != test.A {
-        t.Errorf("1) res.A + test.A was incorrect, got: %d, want: %d", res.A, test.A)
-    }
+	res = calcMidColor([]color.Color{color.RGBA{1, 1, 1, 1}, color.RGBA{3, 3, 3, 3}})
+	test = color.RGBA{2, 2, 2, 2}
+	if res != test {
+		t.Errorf("1) res.A + test.A was incorrect, got: %d, want: %d", res, test)
+	}
 
-    res = calcMidColor(color.RGBA{253,253,253,253}, color.RGBA{255,255,255,255})
-    test = color.RGBA{254,254,254,254}
-    if res.A != test.A {
-        t.Errorf("2) res.A + test.A was incorrect, got: %d, want: %d", res.A, test.A)
-    }
-
-
-
-
+	res = calcMidColor([]color.Color{color.RGBA{253, 253, 253, 253}, color.RGBA{255, 255, 255, 255}})
+	test = color.RGBA{254, 254, 254, 254}
+	if res != test {
+		t.Errorf("2) res.A + test.A was incorrect, got: %d, want: %d", res, test)
+	}
 }

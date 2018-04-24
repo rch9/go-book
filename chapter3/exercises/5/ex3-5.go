45ab3d4d71e98ce8354c40e36cde796fb589cc3f
Roman <churomann@gmail.com>
2018-04-24 10:06:06 +0300

    added ex3

--- chapter3/exercises/5/ex3-5.go
+++ chapter3/exercises/5/ex3-5.go
@@ -20,11 +20,6 @@ func main() {
         for px := 0; px < width; px++ {
             x := float64(px) / width *(xmax - xmin) + xmin
             z := complex(x, y)
-
-            // OPTIMIZE: x^2 + y^2 > 4
-            if cmplx.Abs(z) > 2 {
-
-            }
             img.Set(px, py, mandelbrot(z))
         }
     }
@@ -39,7 +34,8 @@ func mandelbrot(z complex128) color.Color {
     for n := uint8(0); n < iterations; n++ {
         v = v*v + z
         if cmplx.Abs(v) > 2 {
-            return color.RGBA{255 - contrast*n,255 - contrast*n, 255 - contrast*n, 0}
+            // return color.RGBA{255 - contrast*n, 0, 0, 255}
+            return color.YCbCr{255 - contrast*n, contrast*n, 0}
         }
     }
     return color.Black

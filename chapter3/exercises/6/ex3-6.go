3536242544c00bd874ff5c8d10448598b06959f3
Roman <churomann@gmail.com>
2018-04-24 12:38:41 +0300

    added wrong 3.6

--- chapter3/exercises/6/ex3-6.go
+++ chapter3/exercises/6/ex3-6.go
@@ -16,36 +16,38 @@ func main() {
     )
 
     img := image.NewRGBA(image.Rect(0, 0, width, height))
-    for py := 0; py < height; py++ {
-        y := float64(py) / height * (ymax - ymin) + ymin //?
-        for px := 0; px < width; px++ {
-            x := float64(px) / width *(xmax - xmin) + xmin
-            z := complex(x, y)
-            img.Set(px, py, mandelbrot(z)) // TODO: add sum of mandelbrot / 4
+    for py := 0; py < height; py += 4 {
+        y0 := float64(py + 0) / height * (ymax - ymin) + ymin
+        y1 := float64(py + 1) / height * (ymax - ymin) + ymin
+        y2 := float64(py + 2) / height * (ymax - ymin) + ymin
+        y3 := float64(py + 3) / height * (ymax - ymin) + ymin
+        for px := 0; px < width; px += 4 {
+            x0 := float64(px + 0) / width *(xmax - xmin) + xmin
+            x1 := float64(px + 1) / width *(xmax - xmin) + xmin
+            x2 := float64(px + 2) / width *(xmax - xmin) + xmin
+            x3 := float64(px + 3) / width *(xmax - xmin) + xmin
+
+            z0 := complex(x0, y0)
+            z1 := complex(x1, y1)
+            z2 := complex(x2, y2)
+            z3 := complex(x3, y3)
+            img.Set(px, py, calcMidColor([]color.Color{
+                mandelbrot(z0), mandelbrot(z1), mandelbrot(z2), mandelbrot(z3)}))
         }
     }
     png.Encode(os.Stdout, img) // should check errors
 }
 
-// func supersampling(colors [4]color.Color) (res color.Color) {
-//     var r, g, b, a uint
-//
-//     for color := range colors {
-//         res.R
-//     }
-//
-//     return res{r, g, b, a}
-// }
+func calcMidColor(colors []color.Color) color.Color {
+    var r, g, b, a uint32
+    for _, c := range colors {
+        cr, cg, cb, ca := c.RGBA()
+        r, g, b, a = r + cr, g + cg, b + cb, a + ca
+    }
+    len := uint32(len(colors))
+    r, g, b, a = r / len, g / len, b / len, a / len
 
-// TODO: check pointers
-// TODO: check syntax
-func sumColors(rgba1, rgba2 color.RGBA) color.RGBA {
-    rgba1.R += rgba2.R
-    rgba1.G += rgba2.G
-    rgba1.B += rgba2.B
-    rgba1.A += rgba2.A
-
-    return rgba1
+    return color.RGBA{uint8(r), uint8(g), uint8(b), uint8(a)}
 }
 
 func mandelbrot(z complex128) color.Color {
@@ -56,7 +58,7 @@ func mandelbrot(z complex128) color.Color {
     for n := uint8(0); n < iterations; n++ {
         v = v*v + z
         if cmplx.Abs(v) > 2 {
-            return color.Gray{255 - contrast*n}
+            return color.YCbCr{255 - contrast*n, contrast*n, 255 - contrast*n}
         }
     }
     return color.Black
--- chapter3/exercises/6/ex3-6_test.go
+++ chapter3/exercises/6/ex3-6_test.go
@@ -1,9 +1,12 @@
 package main
 
-import "testing"
+import (
+    "testing"
+    "image/color"
+)
 
 func TestSupersampling(t *testing.T) {
-    t.Logf("test %d stated\n", 1)
+    t.Log("TestSupersampling stated")
     // var color int
     //
     // color = getColorFromRangeByNormedAvg(0)
@@ -17,17 +20,23 @@ func TestSupersampling(t *testing.T) {
     // }
 }
 
-func TestSumColors(t *testing.T) {
-    t.Logf("test %d stated\n", 1)
-    // var color int
-    //
-    // color = getColorFromRangeByNormedAvg(0)
-    // if color != 0x0000ff {
-    //     t.Errorf("color0 was incorrect, got: %#.6x, want: %#.6x.", color, 0x0000ff)
-    // }
-    //
-    // color = getColorFromRangeByNormedAvg(1)
-    // if color != 0xff0000 {
-    //     t.Errorf("color1 was incorrect, got: %#.6x, want: %#.6x.", color, 0xff0000)
-    // }
+func TestCalcMidColor(t *testing.T) {
+    t.Log("TestCalcMidColor stated")
+    var res, test color.RGBA
+
+    res = calcMidColor(color.RGBA{1,1,1,1}, color.RGBA{3,3,3,3})
+    test = color.RGBA{2,2,2,2}
+    if res.A != test.A {
+        t.Errorf("1) res.A + test.A was incorrect, got: %d, want: %d", res.A, test.A)
+    }
+
+    res = calcMidColor(color.RGBA{253,253,253,253}, color.RGBA{255,255,255,255})
+    test = color.RGBA{254,254,254,254}
+    if res.A != test.A {
+        t.Errorf("2) res.A + test.A was incorrect, got: %d, want: %d", res.A, test.A)
+    }
+
+
+
+
 }

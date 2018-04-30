382f98f950e45e4088791194786bcbda4f513956
Roman <churomann@gmail.com>
2018-04-30 17:26:41 +0300

    added ex4-7

---
+++ chapter4/exercises/ex4-7/ex4-7.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := "123"
+	fmt.Println(s)
+	b := reverse([]byte(s))
+	fmt.Printf("%s\n", b)
+	//нужна память под byte, срез по string тоже string
+}
+
+func reverse(b []byte) []byte {
+	for i, j := 0, len(b)-1; i < j; i, j = i+1, j-1 {
+		b[i], b[j] = b[j], b[i]
+	}
+	return b
+}

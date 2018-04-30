f90d92594649a3c56dafe468f79190c2dc6b1f01
Roman <churomann@gmail.com>
2018-04-30 16:47:10 +0300

    added ex4-5

--- chapter4/exercises/ex4-5/ex4-5.go
+++ chapter4/exercises/ex4-5/ex4-5.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	s := []int{0, 0, 0, 1, 1, 1, 1, 2, 3, 4, 4}
+	fmt.Println(removeDups(s))
+}
+
+func removeDups(s []int) []int {
+	for i := 0; i < len(s)-1; i++ {
+		if s[i] == s[i+1] {
+			s = remove(s, i)
+			i--
+		}
+	}
+	return s
+}
+
+func remove(slice []int, i int) []int {
+	copy(slice[i:], slice[i+1:])
+	return slice[:len(slice)-1]
 }

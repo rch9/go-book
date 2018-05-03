3d1551456d52fd1cd20af228998543135c05047c
Roman <churomann@gmail.com>
2018-05-03 14:01:02 +0300

    fixed 3-12

--- chapter3/exercises/12/ex3-12.go
+++ chapter3/exercises/12/ex3-12.go
@@ -9,14 +9,14 @@ func main() {
 	s1 := "stop"
 	s2 := "post"
 
-	fmt.Println(chackAnogramm(s1, s2))
+	fmt.Println(isAnogramm(s1, s2))
 
 	s1 = "stop"
 	s2 = "ssop"
-	fmt.Println(chackAnogramm(s1, s2))
+	fmt.Println(isAnogramm(s1, s2))
 }
 
-func chackAnogramm(s1, s2 string) bool { //ASCII
+func isAnogramm(s1, s2 string) bool { //ASCII
 	if len(s1) == len(s2) {
 		for _, v := range s1 {
 			if !strings.Contains(s2, string(v)) {
